e3acae336758c97e3ac2caeb6295d174c935d465
Rika <janerikazhang@gmail.com>
2017-09-07 22:17:45 +0800

    bubble sort

--- src/main/java/interview/prepare/Sorting.java
+++ src/main/java/interview/prepare/Sorting.java
@@ -8,21 +8,24 @@ import java.util.Arrays;
 public class Sorting {
     public static void main(String[] args) {
         Sorting sorting = new Sorting();
-        int[] arr = sorting.insertionSort(new int[] {2,1,3,5,4});
+        int[] arr = sorting.insertionSort(new int[]{2, 1, 3, 5, 4});
         System.out.println(Arrays.toString(arr));
 
-        arr = sorting.countingSort(new int[] {2,1,3,5,4}, 10);
+        arr = sorting.countingSort(new int[]{2, 1, 3, 5, 4}, 10);
         System.out.println(Arrays.toString(arr));
 
-        arr = sorting.radixSort(new int[] {2,111,3,50,4}, 3);
+        arr = sorting.radixSort(new int[]{2, 111, 3, 50, 4}, 3);
         System.out.println(Arrays.toString(arr));
 
-        arr = sorting.bucketSort(new int[]{33,55,22,11,55}, 56);
+        arr = sorting.bucketSort(new int[]{33, 55, 22, 11, 55}, 56);
+        System.out.println(Arrays.toString(arr));
+
+        arr = sorting.bubbleSort(new int[]{33, 55, 22, 11, 55});
         System.out.println(Arrays.toString(arr));
     }
 
     public int[] insertionSort(int[] arr) {
-        for (int i=1 ; i< arr.length ; i++) {
+        for (int i = 1; i < arr.length; i++) {
             int key = arr[i];
             int j = i - 1;
             while (j >= 0 && arr[j] > key) {
@@ -35,7 +38,7 @@ public class Sorting {
     }
 
     public int[] radixSort(int[] arr, int d) {
-        for (int i=1; i<=d; i++) {
+        for (int i = 1; i <= d; i++) {
             arr = countingSortforRadix(arr, i);
         }
         return arr;
@@ -43,20 +46,20 @@ public class Sorting {
 
     private int[] countingSortforRadix(int[] arr, int radix) {
         int[] countArr = new int[200];
-        for (int i=0; i<countArr.length; i++) {
+        for (int i = 0; i < countArr.length; i++) {
             countArr[i] = 0;
         }
-        for (int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             int digit = getDigitForIndex(arr[i], radix);
             countArr[digit] = countArr[digit] + 1;
         }
-        for (int i=1; i<countArr.length; i++) {
-            countArr[i] =  countArr[i] + countArr[i-1];
+        for (int i = 1; i < countArr.length; i++) {
+            countArr[i] = countArr[i] + countArr[i - 1];
         }
         int[] sortedArr = new int[arr.length];
-        for (int i=arr.length-1; i>=0; i--) {
+        for (int i = arr.length - 1; i >= 0; i--) {
             int digit = getDigitForIndex(arr[i], radix);
-            sortedArr[countArr[digit]-1] = arr[i];
+            sortedArr[countArr[digit] - 1] = arr[i];
             countArr[digit] = countArr[digit] - 1;
         }
         System.out.println("radix sort>> " + Arrays.toString(sortedArr));
@@ -64,33 +67,32 @@ public class Sorting {
     }
 
     private int getDigitForIndex(int number, int index) {
-        for (int i=index; i > 1; i --) {
-            number = number/10;
+        for (int i = index; i > 1; i--) {
+            number = number / 10;
         }
         return number % 10;
     }
 
     private int[] countingSort(int[] arr, int range) {
         int[] countArr = new int[range];
-        for (int i=0; i<countArr.length; i++) {
+        for (int i = 0; i < countArr.length; i++) {
             countArr[i] = 0;
         }
-        for (int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             countArr[arr[i]] = countArr[arr[i]] + 1;
         }
-        for (int i=1; i<countArr.length; i++) {
-            countArr[i] =  countArr[i] + countArr[i-1];
+        for (int i = 1; i < countArr.length; i++) {
+            countArr[i] = countArr[i] + countArr[i - 1];
         }
         int[] sortedArr = new int[arr.length];
-        for (int i=arr.length-1; i>=0; i--) {
-            sortedArr[countArr[arr[i]]-1] = arr[i];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sortedArr[countArr[arr[i]] - 1] = arr[i];
             countArr[arr[i]] = countArr[arr[i]] - 1;
         }
         return sortedArr;
     }
 
-    public int[] bucketSort(int[] arr, int maxValue)
-    {
+    public int[] bucketSort(int[] arr, int maxValue) {
         // bucket Sort
         int[] bucket = new int[maxValue + 1];
         int[] sorted_sequence = new int[arr.length];
@@ -107,16 +109,16 @@ public class Sorting {
         return sorted_sequence;
     }
 
-    public Double[] insertionSortDouble(Double[] arr) {
-        for (int i=1 ; i< arr.length ; i++) {
-            double key = arr[i];
-            int j = i - 1;
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-            arr[j + 1] = key;
-        }
+    int[] bubbleSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
         return arr;
     }
 }
